dbe5716e589305222d57c7ef977fc79ca0a7b7d7
zekrythedank <76849029+zekrythedank@users.noreply.github.com>
2021-01-28 17:47:14 -0500

    Site Version 6

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ if (window.sidebar){
 document.onmousedown=killCopy
 document.onclick=reEnable
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0px";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
